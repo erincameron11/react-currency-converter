1765e7dbfd6f422cfcd582a136db624c656d5b69
Erin Cameron <erincameron11@hotmail.com>
2024-04-11 15:24:45 -0400

    add error handling

--- currency-app/src/app/components/CurrencyForm.js
+++ currency-app/src/app/components/CurrencyForm.js
@@ -9,6 +9,7 @@ export const CurrencyForm = () => {
     const [currencyTwo, setCurrencyTwo] = useState();
     const [countryOne, setCountryOne] = useState("");
     const [countryTwo, setCountryTwo] = useState("");
+    const [output, setOutput] = useState("");
 
     // Define variables
     var rate = 0;
@@ -30,9 +31,21 @@ export const CurrencyForm = () => {
             rate = data[countryOne][countryTwo];
             // Calculate the total
             total = (currencyOne * rate).toFixed(2);
-            // Set the second currency
-            setCurrencyTwo(total);
-        });        
+            // Set the output, if total is a number value
+            if(!isNaN(total)) {
+                // Set the second currency
+                setCurrencyTwo(total);
+                // Set the output
+                setOutput(currencyOne + " " + countryOne.toUpperCase() + " equals " + total + " " + countryTwo.toUpperCase());
+            } else {
+                // Set an error output
+                setOutput("*** Please enter a valid country ==> \'" + countryTwo + "\' is not in the database ***");
+            }
+        })
+        .catch (error => {
+            console.log("URL Error ==> Please enter a valid country\n" + error);
+            setOutput("*** Please enter a valid country ==> \'" + countryOne + "\' is not in the database ***");
+        });    
     }
 
     // Function to handle the first Currency entered text
@@ -44,16 +57,17 @@ export const CurrencyForm = () => {
     // Function to handle the first Currency entered text
     const handleCountryOne = e => {
         // Set the first country state
-        setCountryOne(e.target.value);
+        setCountryOne(e.target.value.toLowerCase());
     }
 
     // Function to handle the second Currency entered text
     const handleCountryTwo = e => {
         // Set the second country state
-        setCountryTwo(e.target.value);
+        setCountryTwo(e.target.value.toLowerCase());
     }
 
     return (
+        <div>
         <form onSubmit={handleConvert}>
             <input className="currency" placeholder="1.00" pattern="\d{1,9}\.?\d{2}?" onChange={handleCurrencyOne} value={currencyOne} required/>
             <input className="currencyCountry" placeholder="eg. CAD" onChange={handleCountryOne} value={countryOne} required/>
@@ -64,6 +78,7 @@ export const CurrencyForm = () => {
             <input className="currencyCountry" placeholder="eg. USD" onChange={handleCountryTwo} value={countryTwo} required/>
             <button type="submit">Convert</button>
         </form>
-        
+        <p className="output">{output}</p>
+        </div>
     );
 };
